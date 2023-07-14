2f662ea1f0054ebba44c832751f0fecbf16a26af
PotatoPobre69 <85847784+PotatoPobre69@users.noreply.github.com>
2023-07-14 20:49:17 -0300

    Update ex02.dart
    add the codes to put the numbers in decreased order

--- ex02.dart
+++ ex02.dart
@@ -1,3 +1,12 @@
 /// Exercício 02 - Contagem Regressiva:
 /// Escreva uma função que utilize um laço de repetição para 
 /// imprimir os números de 10 a 1.
+
+void imprimirDecrescente1a10() {
+  List<num> numeros = [];
+  for (var i = 1; i < 11; i++) {
+    numeros.add(i);
+  }
+  numeros.sort();
+  print('${numeros.reversed}');
+}
