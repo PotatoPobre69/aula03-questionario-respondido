69eb5ff969f53c45fa3cee8132ced735fb7ebea0
PotatoPobre69 <85847784+PotatoPobre69@users.noreply.github.com>
2023-07-14 21:52:44 -0300

    Update ex06.dart
    add codes

--- ex06.dart
+++ ex06.dart
@@ -1,3 +1,11 @@
 /// Exercício 06 - Média de uma Lista de Números:
 /// Escreva uma função que receba uma lista de números como parâmetro 
 /// e retorne a média desses números.
+
+listadeNumeros() {
+  List<int> numeroslista = [1,2,3,4,5,6,7,8,9,10];
+  int numerossoma = 0;
+  numeroslista.forEach((numero) => (numerossoma+=numero));
+  num media = numerossoma / (numeroslista.length);
+  print('A média dos numeros da lista é $media');
+}
