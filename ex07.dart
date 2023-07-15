45c4cfa3e95c20af970a3ac73a76b173e38fdcba
PotatoPobre69 <85847784+PotatoPobre69@users.noreply.github.com>
2023-07-15 09:28:29 -0300

    Update ex07.dart
    add fibonacci code

--- ex07.dart
+++ ex07.dart
@@ -1,3 +1,22 @@
 /// Exercício 07 - Números Fibonacci:
 /// Escreva uma função que receba um número inteiro como parâmetro e 
 /// retorne uma lista contendo a sequência de Fibonacci até esse número.
+
+int fibonacci(int n) {
+  if (n == 0 || n == 1) {
+    return n;
+  } 
+  else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  } 
+}
+
+void ImprimirFibonacci() {
+  print('Defina a quantidade de integrantes da sequência de fibonacci que serão mostrados:');
+  int n = int.parse(stdin.readLineSync()!);
+  List<dynamic> lista = [];
+  for (int i = 0; i < n; i++) {
+    lista.add(fibonacci(i));
+  }
+  print("Sequência de Fibonacci: $lista");
+}
