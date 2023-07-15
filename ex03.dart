fbdd49ea2807aff5d8a8dc430fe9f88630fb8c16
PotatoPobre69 <85847784+PotatoPobre69@users.noreply.github.com>
2023-07-14 21:17:28 -0300

    Update ex03.dart
    add the codes

--- ex03.dart
+++ ex03.dart
@@ -1,3 +1,20 @@
 /// Exercício 03 - Verificação de Número Primo:
 /// Escreva uma função que receba um número inteiro como parâmetro 
 /// e retorne verdadeiro se o número for primo e falso caso contrário.
+
+import 'dart:io';
+
+verificarNumeroPrimo (){
+    print('Adicione um número:');
+    int numero = int.parse(stdin.readLineSync()!);
+  if (numero <= 1){
+    print('Falso');
+  }
+  for (int i = 2; i <= numero / 2; i++){
+    if (numero % i == 0){
+      print('Falso');
+    } else { 
+      print('Verdadeiro');
+    }
+  }
+}
