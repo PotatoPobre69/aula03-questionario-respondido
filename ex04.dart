f60a8f7207e70172011faaa302a3f97ed2fbe227
PotatoPobre69 <85847784+PotatoPobre69@users.noreply.github.com>
2023-07-14 21:26:28 -0300

    Update ex04.dart
    add somadospares code

--- ex04.dart
+++ ex04.dart
@@ -1,3 +1,13 @@
 /// Exercício 04 - Soma dos Números Pares:
 /// Escreva uma função que utilize um laço de repetição para 
-/// calcular a soma dos números pares de 1 a 100.
+/// calcular a soma dos números pares de 1 a 100.]
+
+somadosPares1a100() {
+  int somadospares = 0;
+  for (int i = 1; i <= 100; i++){
+    if (i % 2 == 0){
+      somadospares += i;
+    }
+  }
+  print('A soma dos pares de 1 à 100 é: $somadospares');
+}
